de594d297bf1c0435bcb5ea5af081bcbbd3b7b05
AbdallahBagato <AbdallahBagato1999@gmail.com>
2025-08-05 10:01:37 +0300

    edite quotes

--- Application-Code/backend/__tests__/app.test.js
+++ Application-Code/backend/__tests__/app.test.js
@@ -1,16 +1,16 @@
-const request = require('supertest');
-const express = require('express');
-const app = require('../index'); // محتاج تعمل export للـ app من index.js
+const request = require("supertest");
+const express = require("express");
+const app = require("../index"); // محتاج تعمل export للـ app من index.js
 
 describe('Health Check Endpoints', () => {
   it('GET /healthz should return 200 OK', async () => {
-    const res = await request(app).get('/healthz');
+    const res = await request(app).get("/healthz");
     expect(res.statusCode).toEqual(200);
     expect(res.text).toBe("Healthy");
   });
 
   it('GET /started should return 200 OK', async () => {
-    const res = await request(app).get('/started');
+    const res = await request(app).get("/started");
     expect(res.statusCode).toEqual(200);
     expect(res.text).toBe("Started");
   });
